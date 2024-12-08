8cc1bf43829bad82346f2af7e1f38d3a5e3fe9e2
erwan <erwancherel.pro@gmail.com>
2024-12-08 17:58:51 +0100

    Add CORS header

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"myapi/router"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowMethods: "GET, POST, PATCH, DELETE",
+	}))
+
 	database.ConnectDB()
 
 	router.SetupRoutes(app)
 
-	app.Listen(":5000")
+	app.Listen(":5001")
 }
